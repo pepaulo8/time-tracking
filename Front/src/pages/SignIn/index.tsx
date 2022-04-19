2c22e4c20d43c3d4c3a3436f2cfbca65bf4ce554
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-04-19 17:01:37 -0300

    Pagina Login e loginError

--- Front/src/pages/SignIn/index.tsx
+++ Front/src/pages/SignIn/index.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { Button, Text, View, StyleSheet, KeyboardAvoidingView, Image } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAuth } from '../../contexts/auth';
+
+
+const LOGIN_COLOR = '#139DBF'
+const SIGNUP_COLOR = '#25C1E8'
 
 const SignIn: React.FC = () => {
 
@@ -9,30 +14,129 @@ const SignIn: React.FC = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  //const IMAGE_LOGO = require('../../assets/click-clock-logo.png');
+  const IMAGE_LOGO = require('../../assets/Click-clock-logo-preview.png');
+  
 
   function handleSignIn() {
     signIn(email, password);
   }
 
   return (
-    <View>
-      <TextInput 
-      placeholder='email' keyboardType='email-address'
-      onChangeText={value => setEmail(value)}
-      />
-      
-      <TextInput 
-      placeholder='password' secureTextEntry 
-      onChangeText={value => setPassword(value)}
-      />
+    <View style={styles.container}>
+      <View style={styles.containerLogo}>
+          <Image
+            style={styles.imgLogo}
+            source={IMAGE_LOGO}
+          />
+        </View>
+        <View style={styles.containerLogin}>
 
-      { messageError && 
-        <Text>{messageError}</Text> 
-      }
+      <TextInput style={styles.input}
+            placeholder='E-mail' keyboardType='email-address'
+            onChangeText={value => setEmail(value)}
+          />
 
-      <Button title='Sign In' onPress={handleSignIn}/>
+          <TextInput style={styles.input}
+            placeholder='Password' secureTextEntry
+            onChangeText={value => setPassword(value)}
+          />
+
+          {messageError &&
+            <Text style={styles.msgError}>
+              {messageError}
+            </Text>
+          }
+      <TouchableOpacity onPress={handleSignIn} style={styles.btnLogin}>
+            <Text style={styles.btnTitle}>Login</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.SignUpText}>
+            Don't have an account?
+          </Text>
+          <TouchableOpacity style={styles.btnSignup}
+            onPress={handleSignIn}
+          >
+            <Text style={styles.btnTitle}>Sign Up</Text>
+      </TouchableOpacity>
+        </View>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: '5%',
+    backgroundColor: '#ffffff'
+  },
+  containerLogo: {
+    flex: 3,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    //backgroundColor: 'blue'
+  },
+  imgLogo: {
+    width: 160,
+    height: 155,
+  },
+  containerLogin: {
+    flex: 5,
+    justifyContent: 'center',
+    //backgroundColor: 'green',
+    marginBottom: '15%',
+  },
+  input: {
+    alignSelf: 'center',
+    backgroundColor: '#F1F5F4',
+    borderRadius: 20,
+    fontSize: 18,
+    marginBottom: 10,
+    paddingHorizontal: 20,
+    width: '90%',
+    height: 45,
+  },
+  btnLogin: {
+    backgroundColor: LOGIN_COLOR,
+    borderRadius: 15,
+    width: '90%',
+    height: 45,
+    marginTop: 10,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnSignup: {
+    backgroundColor: SIGNUP_COLOR,
+    borderRadius: 15,
+    width: '90%',
+    height: 45,
+    marginTop: 10,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  SignUpText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    alignSelf: 'center',
+    color: '#A0A0A0'
+  },
+  btnTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFFFFF'
+  },
+  msgError: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#EC1111',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
 
 export default SignIn;
--- Front/src/routes/auth.routes.tsx
+++ Front/src/routes/auth.routes.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import SignIn from '../pages/SignIn';
+import { Image } from 'react-native';
 
 const AuthStack = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
     <AuthStack.Navigator>
-        <AuthStack.Screen name="SignIn" component={SignIn}/>
+        <AuthStack.Screen 
+        name="Login" component={SignIn}
+        options={{
+            headerStyle: {
+                backgroundColor: '#F1F5F4',
+              },
+            headerTintColor: '#139DBF',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 24,
+              //fontFamily: Roboto
+            },
+            headerTitleAlign: 'center'
+          }}
+        />
     </AuthStack.Navigator>
 );
 
