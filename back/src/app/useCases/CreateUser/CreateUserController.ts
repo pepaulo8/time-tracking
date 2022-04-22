8d0b84ad488adfd196c3d49bb15e952a7b2dfc67
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-04-22 12:52:54 -0300

    Validação formato e-mail

--- back/src/app/useCases/CreateUser/CreateUserController.ts
+++ back/src/app/useCases/CreateUser/CreateUserController.ts
@@ -6,23 +6,30 @@ import { CreateUserUseCase } from './CreateUserUseCase'
 export class CreateUserController {
 
 
-    async handle(request: Request, response: Response): Promise<Response>{
+    async handle(request: Request, response: Response): Promise<Response> {
 
         const { name, email, password } = request.body;
 
         const createUserUseCase = container.resolve(CreateUserUseCase)
 
-        const result = await createUserUseCase.execute({name, email, password}) 
-        const existsErrorField = typeof result === 'object'
+        if (createUserUseCase.validateEmail(email)) {
+            const result = await createUserUseCase.execute({ name, email, password })
+            const existsErrorField = typeof result === 'object'
 
-        if(result instanceof AppError){
-            return response.status(400).json(result)
-        } else if(existsErrorField){
-            return response.status(400).json(result)
+            if (result instanceof AppError) {
+                return response.status(400).json(result)
+            } else if (existsErrorField) {
+                return response.status(400).json(result)
+            }
+
+            return response.status(201).json({
+                message: "User registered successfully"
+            })
+        } else {
+            return response.status(400).json({
+                message: "E-mail format provided is invalid"
+            })
         }
-        
-        return response.status(201).json({
-            message: "User registered successfully"
-        })
+
     }
 }
--- back/src/app/useCases/CreateUser/CreateUserUseCase.ts
+++ back/src/app/useCases/CreateUser/CreateUserUseCase.ts
@@ -57,4 +57,12 @@ export class CreateUserUseCase {
         }
 
     }
+
+    validateEmail = (email: string) => {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
 }
