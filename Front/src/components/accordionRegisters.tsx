55431798b5c8a5b6c509143c74a5a91854aa86d8
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-04-29 11:53:28 -0300

    Destaque para dias que ultrapassam 8h

--- Front/src/components/accordionRegisters.tsx
+++ Front/src/components/accordionRegisters.tsx
@@ -7,45 +7,63 @@ const { width } = Dimensions.get('window')
 interface register {
     date: string;
     time: string;
-    type: string; 
+    type: string;
 }
 
 type PropsLR = { data: register[] }
 
 const AccordionRegisters: React.FC<PropsLR> = ({ data }) => {
 
-    const renderAccordians=(data: register[] | any[][])=> {
-    const accordians = [];
-    const qtdDates = Object.keys(data).length - 1;
-    for (let idx = 0; idx < qtdDates; idx++) {
-        let dataDay = data[idx][0]
-        accordians.push(
-        <List.Accordion title={dataDay.date} id={idx+1} key={idx+1}>
-            { renderListItem(dataDay) }
-        </List.Accordion>
-        );
-    }
-    return accordians;
+    const minutesToHours = (periodMinutesWorked: number) => {
+        const hours = Math.floor((periodMinutesWorked/60))
+        const minutes = periodMinutesWorked % 60
+
+        return `${hours}:${minutes < 10 ? '0' + minutes : minutes}`
     }
 
-    const renderListItem=(dataDay: { registers: string | any[]; date: any; })=> {
-    const items = [];
-
-    for (let r = 0; r < dataDay.registers.length; r++) {
-        items.push(
-            <List.Item 
-                key={`${dataDay.date}_${dataDay.registers[r].time} `} 
-                title={dataDay.registers[r].time} 
-                description={dataDay.registers[r].type}
-            />
-        );
+    const renderAccordians = (data: register[] | any[][]) => {
+        const accordians = [];
+        const qtdDates = Object.keys(data).length - 1;
+        for (let idx = 0; idx < qtdDates; idx++) {
+            let dataDay = data[idx][0]
+            let overworkedDay = dataDay.overworked
+            let periodHoursWorked = minutesToHours(dataDay.minutesWorked)
+            accordians.push(
+                <List.Accordion
+                    style={overworkedDay ? { backgroundColor: 'red' } : false}
+                    title={dataDay.date}
+                    description={periodHoursWorked}
+                    right={() => (
+                        <Text>R</Text>
+                    )}
+                    id={idx + 1}
+                    key={idx + 1}
+                >
+                    {renderListItem(dataDay)}
+                </List.Accordion>
+            );
+        }
+        return accordians;
     }
-    return items;
+
+    const renderListItem = (dataDay: { registers: string | any[]; date: any; }) => {
+        const items = [];
+
+        for (let r = 0; r < dataDay.registers.length; r++) {
+            items.push(
+                <List.Item
+                    key={`${dataDay.date}_${dataDay.registers[r].time} `}
+                    title={dataDay.registers[r].time}
+                    description={dataDay.registers[r].type}
+                />
+            );
+        }
+        return items;
     }
 
     return (
         <List.AccordionGroup>
-            { renderAccordians(data) }
+            {renderAccordians(data)}
         </List.AccordionGroup>
     );
 }
