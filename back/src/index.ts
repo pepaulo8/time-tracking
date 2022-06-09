e30bf10ae9008cd788abe1eb2860d06ec30fdaaf
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-06-09 11:40:29 -0300

    comentário para rodar migrations

--- back/src/index.ts
+++ back/src/index.ts
@@ -4,5 +4,7 @@ import { User } from "./app/models/entity/User"
 AppDataSource.initialize().then(async () => {
 
     console.log("Conectado ao BD")
+    //AppDataSource.runMigrations()
 
 }).catch(error => console.log(error))
+
